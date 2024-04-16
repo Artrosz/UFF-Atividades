d3bfe568fb00e6ecec0f2c5d43602ffe98db8a99
Artrosz <Artrneto@gmail.com>
2024-04-16 15:40:34 -0300

    Programa para inverter a ordem de um numero dado

---
+++ Atividade em aula/Inversão de números.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int numero, decomposicao;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    while(numero != 0)
+    {
+    decomposicao = numero % 10;
+    printf("%d", decomposicao);
+    numero = numero / 10;
+    }
+
+    return 0;
+}
