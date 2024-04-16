040fcf517754ae0bfa9a2aa5c977c8dcc9666b7f
Artrosz <Artrneto@gmail.com>
2024-04-15 21:27:45 -0300

    Programa para verificar se um numero dado é primo

--- Lista2 prog/L2-8.c
+++ Lista2 prog/L2-8.c
@@ -2,13 +2,27 @@
 #include <stdio.h>
 
 int main (){
-    int numero;
+    int numero, contador = 2, detector = 0, aux;
 
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
 
+    while(contador < numero){
 
+    if(numero % contador == 0){
+        detector++;
+    } 
+    contador++;
 
+    }
 
+    if(detector != 0){
+        printf("%d Nao eh um numero primo", numero);
+    }
 
+    else{
+        printf("%d Eh um numero primo", numero);        
+    }
 
     return 0;
 }
