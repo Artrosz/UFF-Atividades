580b8fa0b82ebed3e74c503f60b374b68b935385
Artrosz <Artrneto@gmail.com>
2024-04-18 18:56:06 -0300

    Aula 5

--- Aula 5/A5 P2-3.c
+++ Aula 5/A5 P2-3.c
@@ -2,17 +2,20 @@
 #include <stdio.h>
 
 int main(){
-int numero1, resultado, digitos = 0, soma, contador;
+int numero1, resultado, digitos = 0, soma, acumulo;
 
 printf("\n Digite um numero: ");
 scanf("%d", &numero1);
 
+
+
 while (numero1 != -1){
 
+acumulo = acumulo + numero1;
 digitos++; 
 resultado = numero1*2;
-soma = numero1 + contador;
-contador = numero1;
+
+
 
 
 printf("\n O resultado eh: %d", resultado);
@@ -22,6 +25,7 @@ scanf ("%d", &numero1);
 
 }
 printf("\n O numero de digitos foram:%d", digitos);
+printf("A combinacao dos numeros selecionados eh: %d",acumulo);
 
 
 return 0;
