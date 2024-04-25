0f2a23d7b0cc5640467bf8f658f1d97bbbace447
Artrosz <Artrneto@gmail.com>
2024-04-24 23:38:18 -0300

    Menor nota

---
+++ Aula 7/A7 Menor nota.c
@@ -0,0 +1,25 @@
+// Data: 24/04/2024
+/* Usando vetor, recebemos nota de alunos e irá printar a maior nota, a menor nota, calcular e printar a media
+ e mostrar quantos alunos irão para a vs.*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (){
+    
+   int maior = 0, menor = 100;
+    int i = 0, notas[3]; 
+
+    for (i = 0; i < 3; i++){
+    printf("Digita: ");
+    scanf("%d", &notas[i]);
+        if (notas [i] < menor){
+            menor = notas[i];
+
+        }
+    }
+    printf("\nMenor  valor: %d", menor);
+
+    
+    return 0;
+}
